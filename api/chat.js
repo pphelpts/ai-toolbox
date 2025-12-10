fce9f8359ce87210518665ab2c0c5bdd3e13e295
pphelpts <2273454550@qq.com>
2025-12-10 23:50:48 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function (req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
@@ -32,4 +32,4 @@ export default async function handler(req, res) {
     console.error(error);
     return res.status(500).json({ error: "Request failed" });
   }
-}
+};
